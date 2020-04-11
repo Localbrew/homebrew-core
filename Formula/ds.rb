beba59a7b585ee76c3a94ef6d73499054882a320
aerobounce <10491362+aerobounce@users.noreply.github.com>
2020-04-12 00:50:42 +0900

    ds: Update hash

--- Formula/ds.rb
+++ Formula/ds.rb
@@ -2,7 +2,7 @@ class Ds < Formula
     desc "Convert defaults or any plist into Shell Scripts"
     homepage "https://github.com/aerobounce/defaults.sh"
     url "https://github.com/aerobounce/defaults.sh/archive/2020.04.10.zip"
-    sha256 "e7458724f861c656725eaad19d78405fb12cca4bee43aa191278530e4b6300a8"
+    sha256 "d90489973e0c3277077cbe12b9ee050ed132beaaf80adde37d541985a4dbed2c"
     head "https://github.com/aerobounce/defaults.sh.git"
 
     bottle :unneeded
